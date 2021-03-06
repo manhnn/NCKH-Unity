0529a4c5e5d1507ea500d87a1f04a5a20eeffa20
PhanDacSon213 <phandacson213@gmail.com>
2021-03-07 05:03:26 +0700

    Final

--- NCKH/Assets/Project/Scripts/Core/EnumFlagsAttributeDrawer.cs
+++ NCKH/Assets/Project/Scripts/Core/EnumFlagsAttributeDrawer.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System;
 using UnityEditor;
 
+#if UNITY_EDITOR
 [CustomPropertyDrawer(typeof(EnumFlagsAttribute))]
 public class EnumFlagsAttributeDrawer : PropertyDrawer
 {
@@ -11,3 +12,4 @@ public class EnumFlagsAttributeDrawer : PropertyDrawer
         _property.intValue = EditorGUI.MaskField(_position, _label, _property.intValue, _property.enumNames);
     }
 }
+#endif
